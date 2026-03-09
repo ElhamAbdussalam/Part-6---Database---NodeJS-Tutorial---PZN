3dd3c2149d58024ef769c2a87c4d82185c4ca076
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-09 20:53:54 +0700

    update

--- test/crud.test.js
+++ test/crud.test.js
@@ -4,6 +4,7 @@ describe("Prisma Client", () => {
   beforeEach(async () => {
     await prismaClient.customer.deleteMany();
   });
+
   it("should be able to create customer", async () => {
     const customer = await prismaClient.customer.create({
       data: {
@@ -17,6 +18,33 @@ describe("Prisma Client", () => {
     expect(customer.id).toBe("Elham1");
     expect(customer.email).toBe("elham1@email.com");
     expect(customer.name).toBe("Elham1");
+    expect(customer.phone).toBe("089999999999");
+  });
+
+  it("should be able to update customer", async () => {
+    // buat data dulu
+    await prismaClient.customer.create({
+      data: {
+        id: "Elham1",
+        email: "elham1@email.com",
+        name: "Elham1",
+        phone: "089999999999",
+      },
+    });
+
+    // lalu update
+    const customer = await prismaClient.customer.update({
+      where: {
+        id: "Elham1",
+      },
+      data: {
+        name: "M Elham Abdussalam",
+      },
+    });
+
+    expect(customer.id).toBe("Elham1");
+    expect(customer.email).toBe("elham1@email.com");
+    expect(customer.name).toBe("M Elham Abdussalam");
     expect(customer.phone).toBe("089999999999");
   });
 });
