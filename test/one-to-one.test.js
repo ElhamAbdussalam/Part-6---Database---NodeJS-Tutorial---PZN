b7b4b4ea0be723229bb82f34f1f479711afc833a
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-10 18:47:44 +0700

    one to one

---
+++ test/one-to-one.test.js
@@ -0,0 +1,24 @@
+import { prismaClient } from "../prisma-client";
+
+describe("One to one", () => {
+  it("should be able to create one to one", async () => {
+    const customer = await prismaClient.customer.create({
+      data: {
+        id: "elham",
+        name: "Elham",
+        email: "elham@gmail.com",
+        phone: "08123456789",
+      },
+    });
+
+    const wallet = await prismaClient.wallet.create({
+      data: {
+        id: "wallet1",
+        balance: 1000,
+        customer_id: customer.id,
+      },
+    });
+
+    console.info(wallet);
+  });
+});
