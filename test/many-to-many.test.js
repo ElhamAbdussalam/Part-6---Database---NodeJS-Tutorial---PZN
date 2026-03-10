8a2ca5604be826bb480d10c350c680c34fe6d1f2
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-10 20:24:17 +0700

    many to many

--- test/many-to-many.test.js
+++ test/many-to-many.test.js
@@ -1,52 +1,71 @@
 import { prismaClient } from "../prisma-client";
 
-describe("Many to many", () => {
+// describe("Many to many explicit", () => {
+//   it("should be able to many to many", async () => {
+//     // const like = await prismaClient.like.create({
+//     //   data: {
+//     //     customer_id: "eko",
+//     //     product_id: "P0001",
+//     //   },
+//     //   include: {
+//     //     customer: true,
+//     //     product: true,
+//     //   },
+//     // });
+//     // console.info(like);
+
+//     // const customer = await prismaClient.customer.findUnique({
+//     //   where: {
+//     //     id: "eko",
+//     //   },
+//     //   include: {
+//     //     likes: {
+//     //       include: {
+//     //         product: true,
+//     //       },
+//     //     },
+//     //   },
+//     // });
+//     // console.info(JSON.stringify(customer));
+
+//     const customer = await prismaClient.customer.findMany({
+//       where: {
+//         likes: {
+//           some: {
+//             product: {
+//               name: {
+//                 contains: "A",
+//               },
+//             },
+//           },
+//         },
+//       },
+//       include: {
+//         likes: {
+//           include: {
+//             product: true,
+//           },
+//         },
+//       },
+//     });
+//   });
+// });
+
+describe("Many to many implicit", () => {
   it("should be able to many to many", async () => {
-    // const like = await prismaClient.like.create({
-    //   data: {
-    //     customer_id: "eko",
-    //     product_id: "P0001",
-    //   },
-    //   include: {
-    //     customer: true,
-    //     product: true,
-    //   },
-    // });
-    // console.info(like);
-
-    // const customer = await prismaClient.customer.findUnique({
-    //   where: {
-    //     id: "eko",
-    //   },
-    //   include: {
-    //     likes: {
-    //       include: {
-    //         product: true,
-    //       },
-    //     },
-    //   },
-    // });
-    // console.info(JSON.stringify(customer));
-
-    const customer = await prismaClient.customer.findMany({
+    const customer = await prismaClient.customer.update({
       where: {
-        likes: {
-          some: {
-            product: {
-              name: {
-                contains: "A",
-              },
-            },
-          },
+        id: "eko",
+      },
+      data: {
+        loves: {
+          connect: [{ id: "P0001" }, { id: "P0002" }],
         },
       },
       include: {
-        likes: {
-          include: {
-            product: true,
-          },
-        },
+        loves: true,
       },
     });
+    console.info(JSON.stringify(customer));
   });
 });
