9af0f09ed6d6c95ce7ef9e70f21d810e23f4ce75
ElhamAbdussalam <muhammadelhamcity7@gmail.com>
2026-03-10 19:43:33 +0700

    one to many

--- test/one-to-many.test.js
+++ test/one-to-many.test.js
@@ -2,16 +2,39 @@ import { prismaClient } from "../prisma-client";
 
 describe("One to many", () => {
   it("should be able to one to many", async () => {
-    const comment = await prismaClient.comment.create({
+    // const comment = await prismaClient.comment.create({
+    //   data: {
+    //     customer_id: "eko",
+    //     title: "Insert comment",
+    //     description: "Description comment",
+    //   },
+    //   include: {
+    //     customer: true,
+    //   },
+    // });
+    // console.info(comment);
+
+    const customer = await prismaClient.customer.create({
       data: {
-        customer_id: "eko",
-        title: "Insert comment",
-        description: "Description comment",
-      },
-      include: {
-        customer: true,
+        id: "alex",
+        name: "alex",
+        email: "alex@email",
+        phone: "12345678",
+        comments: {
+          createMany: {
+            data: [
+              {
+                title: "Comment 1",
+                description: "Description comment 1",
+              },
+              {
+                title: "Comment 2",
+                description: "Description comment 2",
+              },
+            ],
+          },
+        },
       },
     });
-    console.info(comment);
   });
 });
